64be4f6384e221bb9366c937feecce9a8b9fca62
DeathBarmaglot <fghjkl11@gmail.com>
2021-11-05 16:51:33 +0200

    add: new testCases for ArrayList

--- src/test/java/com/luxoft/datastructures/arraylist/ArrayListTest.java
+++ src/test/java/com/luxoft/datastructures/arraylist/ArrayListTest.java
@@ -1,114 +1,127 @@
 package com.luxoft.datastructures.arraylist;
 
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
-
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.*;
 
 // Abstract Data Type
 public class ArrayListTest {
-ArrayList arrayList = new ArrayList();
-    @Test
-    public void testAdd(){
-        arrayList.add("A");
-        arrayList.add("B");
-        assertEquals(2,arrayList.size());
-        arrayList.add("A",2);
-        arrayList.add("B",6);
-        assertEquals(4,arrayList.size());
-}
+    ArrayList arrayList = new ArrayList();
 
     @Test
-    public void testRemoveDefault(){
+    public void testAdd() {
+        arrayList.add("A");
+        arrayList.add("B");
+        assertEquals(2, arrayList.size());
+        arrayList.add("A", 2);
+        arrayList.add("B", 6);
+        assertEquals(4, arrayList.size());
+    }
+
+    @Test
+    public void testRemoveDefault() {
         arrayList.add("A");
         arrayList.add("B");
         arrayList.add("C");
-        assertEquals(3,arrayList.size());
+        assertEquals(3, arrayList.size());
         arrayList.remove(0);
-//        assertEquals("A",arrayList.get(0));
-        assertEquals(2,arrayList.size());
-        assertEquals("B",arrayList.get(0));
-        assertEquals("C",arrayList.get(1));
+        assertEquals(2, arrayList.size());
+        assertEquals("B", arrayList.get(0));
+        assertEquals("C", arrayList.get(1));
     }
 
     @Test
-    public void testRemove(){
+    public void testRemoveElements() {
         arrayList.add("A");
-        arrayList.add("B",1);
-        assertEquals(2,arrayList.size());
+        arrayList.add("B", 1);
+        assertEquals(2, arrayList.size());
         arrayList.remove(1);
-        assertEquals(1,arrayList.size());
-        assertEquals("A",arrayList.get(0));
-    }
-
-
-
-
-//    // we can add value by index between [0, size]
-//    // otherwise throw new IndexOutOfBoundsException
-
-//    // we can remove value by index between [0, size - 1]
-//    // otherwise throw new IndexOutOfBoundsException
-//
-//    // [A, B, C] remove = 0
-//    // [B (index = 0) , C (index = 1)]
-//    Object remove(int index);
-//
-//    // [A, B, C, null, null] size = 3
-//    // we can get value by index between [0, size - 1]
-//    // otherwise throw new IndexOutOfBoundsException
-//    Object get(int index);
-//
-//    // we can set value by index between [0, size - 1]
-//    // otherwise throw new IndexOutOfBoundsException
-//    Object set(Object value, int index);
-//
-//    void clear();
-//
-//    int size();
-//
-//    boolean isEmpty();
-//
-//    boolean contains(Object value);
-//
-//    // [A, B, A, C] indexOf(A) -> 0
-//    // -1 if not exist
-//    int indexOf(Object value);
-//
-//    // [A, B, A, C] lastIndexOf(A) -> 2
-//    int lastIndexOf(Object value);
-//
-//    // [A, B, C]
-//    String toString();
-
-    @Test
-    public void testArrayIndexBoundsExceptionLessThenSize(){
-        ArrayList arrayList = new ArrayList();
-        arrayList.add(1, -1);
-        IndexOutOfBoundsException message =
-                assertThrows(IndexOutOfBoundsException.class, () -> {arrayList.add(1,-1);});
-
-        message.getMessage();
-    }
-
-    @Test
-    public void testIndexBoundsExceptionMoreThenSize(){
-        ArrayList arrayList = new ArrayList();
-        arrayList.add(1);
-        IndexOutOfBoundsException message =
-                assertThrows(IndexOutOfBoundsException.class, () -> {arrayList.add(2,2);});
-
-        message.getMessage();
-    }
-
-    @Test
-    public void testThrowsIndexOut(){
-        ArrayList arrayList = new ArrayList();
-        arrayList.add(1);
-        IndexOutOfBoundsException message =
-                assertThrows(IndexOutOfBoundsException.class, () -> {arrayList.get(1);});
         assertEquals(1, arrayList.size());
-        message.getMessage();
-        }
+        assertEquals("A", arrayList.get(0));
     }
+
+    @Test
+    public void testFindLastIndex() {
+        arrayList.add("A");
+        arrayList.add("B");
+        arrayList.add("A");
+        arrayList.add("C");
+        assertEquals(2, arrayList.lastIndexOf("A"));
+    }
+
+    @Test
+    public void testFindFirstIndex() {
+        arrayList.add("A");
+        arrayList.add("B");
+        arrayList.add("A");
+        arrayList.add("C");
+        assertEquals(0, arrayList.indexOf("A"));
+    }
+
+    @Test
+    public void testToString() {
+        arrayList.add("A");
+        arrayList.add("B");
+        arrayList.add("C");
+        assertEquals("[A, B, C]", arrayList.toString());
+    }
+
+    @Test
+    public void testAddClearEmpty() {
+        arrayList.add("A");
+        arrayList.add("B");
+        arrayList.add("C");
+        assertEquals(3, arrayList.size());
+        arrayList.clear();
+        assertEquals(0, arrayList.size());
+        assertTrue(arrayList.isEmpty());
+    }
+
+    @Test
+    public void testContain() {
+        arrayList.add("A");
+        arrayList.add("B");
+        arrayList.add("C");
+        assertTrue(arrayList.contains("A"));
+    }
+
+    @Test
+    public void testIndexBoundsExceptionRemove() {
+        ArrayList arrayList = new ArrayList();
+        arrayList.add(1);
+        IndexOutOfBoundsException message =
+                assertThrows(IndexOutOfBoundsException.class, () -> arrayList.remove(1));
+
+        System.out.println(message.getMessage());
+    }
+
+    @Test
+    public void testArrayIndexBoundsExceptionLessThenSize() {
+        ArrayList arrayList = new ArrayList();
+        arrayList.add(1);
+        IndexOutOfBoundsException message =
+                assertThrows(IndexOutOfBoundsException.class, () -> arrayList.add(1,-1));
+
+        System.out.println(message.getMessage());
+    }
+
+    @Test
+    public void testIndexBoundsExceptionMoreThenSize() {
+        ArrayList arrayList = new ArrayList();
+        arrayList.add(1);
+        IndexOutOfBoundsException message =
+                assertThrows(IndexOutOfBoundsException.class, () -> arrayList.get(2));
+
+        System.out.println(message.getMessage());
+    }
+
+    @Test
+    public void testThrowsIndexOut() {
+        ArrayList arrayList = new ArrayList();
+        arrayList.add(1);
+        assertEquals(1, arrayList.size());
+        IndexOutOfBoundsException message =
+                assertThrows(IndexOutOfBoundsException.class, () -> arrayList.get(2));
+
+        System.out.println(message.getMessage());
+    }
+}
